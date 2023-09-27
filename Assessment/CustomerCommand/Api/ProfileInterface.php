2610c150873e2fcb8caa7e99a0d5c32913b028d3
Dharun Sainath <dharunnsainath@gmail.com>
2023-09-27 18:47:28 +0530

    refactored changes 270923 + profileInterface

--- Assessment/CustomerCommand/Api/ProfileInterface.php
+++ Assessment/CustomerCommand/Api/ProfileInterface.php
@@ -4,6 +4,11 @@ namespace Assessment\CustomerCommand\Api;
 
 interface ProfileInterface
 {
+      /**
+     * Import customer data from an array of data and save it in database.
+     *
+     * @param string $source
+     */
     public function import($source);
 
 }
